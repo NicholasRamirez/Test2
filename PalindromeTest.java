01b5b621dfefd141869fd41851d65a71b5609863
Nicholas A. Ramirez-Ornelas <nar@cpp.edu>
2022-01-31 16:57:58 -0800

    changes made in spacing

---
+++ PalindromeTest.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class PalindromeTest
+{
+   public static void main(String[] args)
+   {
+      Scanner kb = new Scanner(System.in);
+      System.out.print("enter a string: ");
+      String s = kb.nextLine().toLowerCase();
+      System.out.printf("\"%s\" %s a palindrome\n", s,
+                        palindrome(s,0,s.length()-1)? "is": "is not");
+   }
+
+   public static boolean palindrome(String s, int i, int j)
+   {
+      if (i < j)
+         return (s.charAt(i) == s.charAt(j) && palindrome(s, i+1, j-1));
+      else
+         return true;
+   }
+}
+
